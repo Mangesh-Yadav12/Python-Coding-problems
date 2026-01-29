a5f97b7d392ec8e454e9daa92e04e23083bd7058
Mangesh Yadav <mangeshyadavv@gmail.com>
2026-01-29 18:07:51 +0530

    Solved Q2

---
+++ Q2.py
@@ -0,0 +1,10 @@
+"""Check if a number is even or odd"""
+
+def EvenOrOdd(n):
+    if n%2==0:
+        print(f"Number {n} is even")
+    elif n%2 != 0:
+        print(f"Number {n} is odd")
+
+EvenOrOdd(100)
+EvenOrOdd(99)
