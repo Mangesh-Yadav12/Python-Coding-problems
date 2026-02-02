0be65197e314cc4af824d2075f64b1992b4cfc3d
Mangesh Yadav <mangeshyadavv@gmail.com>
2026-02-02 17:57:49 +0530

    Solved Level-3 Q8

---
+++ Basic Python/Phase-1/Level-3/Q8.py
@@ -0,0 +1,12 @@
+"""Check if a number lies within the range [100, 999]."""
+
+def checkNumber(num):
+    if num >= 100 and num <= 999:
+        print("Number lies between the range")
+    else:
+        print("Number not lies between the range")
+
+
+
+checkNumber(567)
+checkNumber(4567)
