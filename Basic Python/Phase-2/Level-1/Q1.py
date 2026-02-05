35553eebd0a493da9613fe8c4f782ff2b3130cb6
Mangesh Yadav <mangeshyadavv@gmail.com>
2026-02-05 21:51:40 +0530

    Solved Print numbers from 1 to 10

---
+++ Basic Python/Phase-2/Level-1/Q1.py
@@ -0,0 +1,7 @@
+"""Print numbers from 1 to 10"""
+
+def printNum():
+    for i in range(1,11):
+        print(i,end=" ")
+
+printNum()
--- DSA/3. Recursion/Q6. Reverse an Array without Loop.py
+++ DSA/3. Recursion/Q6. Reverse an Array without Loop.py
@@ -8,3 +8,17 @@ def func(arr,l,r):
     return arr
 arr= [9,2,3,4,5,4,3,2,3,4,5,6,6]
 print(func(arr,3,7))
+
+
+def function(arr,l,r):
+    l = 0
+    r = len(arr)-1
+    while l >= r:
+        break
+    arr[l],arr[r] = arr[r],arr[l]
+    l += 1
+    r -= 1
+    return arr    
+
+arr= [9,2,3,4,5,4,3,2,3]
+print(function(arr,0,8))
